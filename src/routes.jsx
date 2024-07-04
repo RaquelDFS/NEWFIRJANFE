540c467ff15e66b5afea36a414f5d7919f231d43
Raquel Fonseca <97365473+RaquelDFS@users.noreply.github.com>
2024-07-04 20:43:06 -0300

    projetos parte um

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Projetos from "./pages/Projetos";
 import Contatos from "./pages/Contatos";
 import Page404 from "./pages/Page404";
 import Template from "./pages/Template";
@@ -12,6 +13,7 @@ function AppRoutes() {
         <Route path="/" element={<Template />}>
           <Route index element={<Home />}></Route>
           <Route path="/sobre" element={<Sobre />}></Route>
+          <Route path="/projetos" element={<Projetos />}></Route>
           <Route path="/contatos" element={<Contatos />}></Route>
           <Route path="/*" element={<Page404 />}></Route>
         </Route>
